e957ec5a9fdb4a40c5667a8a90c4d1655c864167
MoKammouni <57012891+MoKammouni@users.noreply.github.com>
2020-02-10 11:58:55 +0100

    Commit proberen is gelukt

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $provincie = "Zuid-Holland";
 $woonplaats = "Den Haag";
 $straat = "Schoeplaan";
 
-echo "Hallo ik ben " .$naam. " , Ik ben " .$leeftijd. " Oud en ben " .$lengte. " meter lang";
+//echo "Hallo ik ben " .$naam. " , Ik ben " .$leeftijd. " Oud en ben " .$lengte. " meter lang";
 
 echo sprintf("Ik ben %s, ik woon in %s. Dat is de provincie %s, op straat %s", $naam, $provincie, $woonplaats, $straat);
 ?>
