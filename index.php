b387c178ee6d038ba33f5f3ec65c421f5312b49b
MoKammouni <57012891+MoKammouni@users.noreply.github.com>
2020-02-10 13:19:15 +0100

    Heel wat code toegevoegd vooral over Arrays

--- index.php
+++ index.php
@@ -10,5 +10,19 @@ $straat = "Schoeplaan";
 
 //echo "Hallo ik ben " .$naam. " , Ik ben " .$leeftijd. " Oud en ben " .$lengte. " meter lang";
 
-echo sprintf("Ik ben %s, ik woon in %s. Dat is de provincie %s, op straat %s", $naam, $provincie, $woonplaats, $straat);
+//echo sprintf("Ik ben %s, ik woon in %s. Dat is de provincie %s, op straat %s", $naam, $provincie, $woonplaats, $straat);
+
+//echo gettype ($leeftijd);
+//$leeftijd2 = (string) $leeftijd;
+//echo gettype($leeftijd2);
+
+
+//Dit is de eerste methode om een array te maken in PHP
+$mijn_array = array($naam, $leeftijd, $lengte, $provincie, $woonplaats, $straat);
+
+//Dit is de tweede methode om een array te maken in PHP
+//$mijn_array = array();
+//array_push($mijn_array, $naam, $leeftijd, $lengte, $provincie, $woonplaats, $straat);
+
+print_r($mijn_array);
 ?>
