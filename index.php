36fb098b0979954668609c89690dc4b80455fa45
MoKammouni <57012891+MoKammouni@users.noreply.github.com>
2020-02-10 11:17:22 +0100

    Nieuwe zin toegevoegd en Sprintf geleerd

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ $naam = "Mohamed";
 $leeftijd = 16;
 $lengte = 1.86;
 
-echo "Hallo ik ben " .$naam. " , Ik ben " .$leeftijd. " Oud en ben " .$lengte. " meter lang";
+$woonplaats = "Den Haag";
+$straat = "Schoeplaan";
 
+//echo "Hallo ik ben " .$naam. " , Ik ben " .$leeftijd. " Oud en ben " .$lengte. " meter lang";
+
+echo sprintf("Ik ben %s, ik woon in %s op straat %s", $naam, $woonplaats, $straat);
 ?>
