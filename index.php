bfcc48b9b0f523220a443dac42a09aa6a10d888f
MoKammouni <57012891+MoKammouni@users.noreply.github.com>
2020-02-10 11:22:18 +0100

    nieuwe variable toegevoegd

--- index.php
+++ index.php
@@ -4,10 +4,11 @@ $naam = "Mohamed";
 $leeftijd = 16;
 $lengte = 1.86;
 
+$provincie = "Zuid-Holland";
 $woonplaats = "Den Haag";
 $straat = "Schoeplaan";
 
 //echo "Hallo ik ben " .$naam. " , Ik ben " .$leeftijd. " Oud en ben " .$lengte. " meter lang";
 
-echo sprintf("Ik ben %s, ik woon in %s op straat %s", $naam, $woonplaats, $straat);
+echo sprintf("Ik ben %s, ik woon in %s. Dat is de provincie %s, op straat %s", $naam, $provincie, $woonplaats, $straat);
 ?>
